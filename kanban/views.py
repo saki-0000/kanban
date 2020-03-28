5ccc6d3be861f038a3a5cc88dd81fed48be3bdc1
saki0000 <saki_27_maruko@yahoo.co.jp>
2020-03-29 08:09:09 +0900

    ホーム画面に不正アクセスできないように

--- kanban/views.py
+++ kanban/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # git@github.com:saki-0000/kanban.git
 def index(request):
     return render(request, "kanban/index.html")
+@login_required
 def home(request):
     return render(request, "kanban/home.html")
 def signup(request):
