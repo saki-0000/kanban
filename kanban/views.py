84b7ebd41a9ff87bfcd5f41b86f4e3d88b04962c
saki0000 <saki_27_maruko@yahoo.co.jp>
2020-03-29 08:37:51 +0900

    import分は名前順

--- kanban/views.py
+++ kanban/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 
 # git@github.com:saki-0000/kanban.git
 def index(request):
