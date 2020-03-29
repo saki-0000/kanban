c764fee683bb4353452c480e9515e1e3489b37f8
saki0000 <saki_27_maruko@yahoo.co.jp>
2020-03-29 13:13:52 +0900

    リストを作成できるようにした

--- kanban/views.py
+++ kanban/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, resolve_url
-from django.views.generic import DetailView, UpdateView
-from .forms import UserForm
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, UpdateView, CreateView
+from .forms import UserForm, ListForm
 from .mixins import OnlyYouMixin
+from .models import List
 
 # git@github.com:saki-0000/kanban.git
 def index(request):
@@ -39,3 +41,12 @@ class UserUpdateView(OnlyYouMixin, UpdateView):
 
     def get_success_url(self):
         return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])
+class ListCreateView(LoginRequiredMixin, CreateView):
+    model = List
+    template_name = "kanban/lists/create.html"
+    form_class = ListForm
+    success_url =reverse_lazy("kanban:home")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
